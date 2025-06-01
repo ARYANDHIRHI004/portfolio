0641f75a854ef483878aa88b092fe3c7d567c32c
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-06-01 17:54:11 +0530

    fix: experience bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Navbar/>
         <About/>
         <Skills/>
-        <Experiance/>
+        {/* <Experiance/> */}
         <Work/>
         <Education/>
         <Contact/>
